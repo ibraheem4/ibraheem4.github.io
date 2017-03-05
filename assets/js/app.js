8ab08013b6a4877482d34311242dfeceaca0df3a
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2017-03-05 12:21:30 -0800

    Snapchat style menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
   $('.ui.sticky')
     .sticky({
       context: '#context'
-    });
+  });
+
+  $(".ui.menu a.item[href='"+window.location.pathname+"']").addClass("active")
 
   $('pre code').each(function(i, block) {
     hljs.highlightBlock(block);
