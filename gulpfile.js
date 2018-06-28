d652db9a1a0f3c78f1c478a2fd15222b893c56a5
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2018-06-27 19:01:36 -0700

    Minify highlight.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
-let gulp = require('gulp');
-let cleanCSS = require('gulp-clean-css');
-let concat = require('gulp-concat');
-let rename = require('gulp-rename');
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const minify = require('gulp-minify');
 
 gulp.task('minify-css', () => {
   return gulp.src(['assets/css/styles.css', 'assets/css/syntax.css'])
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(concat('styles.min.css'))
-    .pipe(gulp.dest('assets/dist'));
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(concat('styles.min.css'))
+  .pipe(gulp.dest('assets/dist'));
+});
+
+gulp.task('minify-js', function() {
+  gulp.src('node_modules/highlight.js/lib/highlight.js')
+  .pipe(minify({
+    ext:{
+      min:'.min.js'
+    },
+    noSource: true
+  }))
+  .pipe(gulp.dest('assets/dist'))
 });
