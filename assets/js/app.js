7c33cc5641720fab24c2c00d17fda0a3a99fbc11
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2020-10-21 00:39:45 -0400

    Prefer em to px, user 16px em size

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,3 @@
 $(document).ready(function() {
   $(".ui.menu a.item[href='"+window.location.pathname+"']").addClass("active")
-
-  $('pre code').each(function(i, block) {
-    hljs.highlightBlock(block);
-  });
 });
